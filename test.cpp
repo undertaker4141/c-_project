a0c019aef6f0666d99492002a5dde18c4c827c36
undertaker4141 <a8925525a@gmail.com>
2024-09-10 01:29:35 +0800

    delete .vscode

--- test.cpp
+++ test.cpp
@@ -1,7 +1,37 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
+class Dog {
+  public:
+    Dog(int n);
+    ~Dog();
+    void Woff();
+    void Eat(string s);
+
+  private:
+    int woffTimes;
+};
+
+Dog::Dog(int n) {
+    this->woffTimes = n;
+}
+
+Dog::~Dog() {}
+
+void Dog::Woff() {
+    for (int i = 0; i < this->woffTimes; i++) {
+        cout << "Woff" << endl;
+    }
+}
+
+void Dog::Eat(string s) {
+    cout << "Eat " << s << endl;
+}
+
 int main() {
-    cout << "Hello world???" << endl;
+    Dog d(5);
+    d.Eat("Dog");
+    d.Woff();
     return 0;
 }
